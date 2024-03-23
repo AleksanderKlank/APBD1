729b74d4796a1591ae78bedad0cdadb0b65e8f51
s27616 <s27616@pjwstk.edu.pl>
2024-03-24 00:00:04 +0100

    wczesna logika kontenrowca

---
+++ Cw2/Cw2/Classes/Kontenerowiec.cs
@@ -0,0 +1,20 @@
+namespace Cw2.Classes;
+
+public class Kontenerowiec
+{
+    private List<Kontener> kontenery;
+    private double predkosc;
+    private int maxKontenerow;
+    private double maksymalnaWaga;
+    
+    public Kontenerowiec(double predkosc, int maxKontenerow, double maksymalnaWaga)
+    {
+        kontenery = new List<Kontener>();
+        this.predkosc = predkosc;
+        this.maxKontenerow = maxKontenerow;
+        this.maksymalnaWaga = maksymalnaWaga''
+    }
+    
+    
+    
+}
