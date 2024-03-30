67a09d4ec9650f65fe0e65ff62e921645dba137c
s27616 <s27616@pjwstk.edu.pl>
2024-03-30 21:35:56 +0100

    dodalem pare testow

--- Cw3/LegacyAppTests/UserServiceTests.cs
+++ Cw3/LegacyAppTests/UserServiceTests.cs
@@ -1,9 +1,80 @@
+using LegacyApp;
+
 namespace LegacyAppTests;
 
 public class UserServiceTests
 {
     [Fact]
-    public void Test1()
+    public void AddUser_Should_Return_False_When_Email_Without_Dot_And_At()
     {
+        //Arrange
+        string firstName = "John";
+        string lastName = "Wick";
+        DateTime dateOfBirth = new DateTime(1973, 2, 1);
+        int clientId = 5;
+        string email = "wick";
+        var service = new UserService();
+        
+        //Act
+        bool result = service.AddUser(firstName, lastName, email, dateOfBirth, clientId);
+        
+        //Assert
+        Assert.Equal(false,result);
     }
+
+    [Fact]
+    public void AddUser_Should_Return_False_When_FirstName_Is_Null()
+    {
+    //Arrange
+    string firstName = "";
+    string lastName = "Wick";
+    DateTime dateOfBirth = new DateTime(1973, 2, 1);
+    int clientId = 5;
+    string email = "wick@wp.pl";
+    var service = new UserService();
+        
+    //Act
+    bool result = service.AddUser(firstName, lastName, email, dateOfBirth, clientId);
+        
+    //Assert
+    Assert.Equal(false,result);
+    } 
+    
+    [Fact]
+    public void AddUser_Should_Return_False_When_LastName_Is_Null()
+    {
+        //Arrange
+        string firstName = "John";
+        string lastName = "";
+        DateTime dateOfBirth = new DateTime(1973, 2, 1);
+        int clientId = 5;
+        string email = "wick@wp.pl";
+        var service = new UserService();
+        
+        //Act
+        bool result = service.AddUser(firstName, lastName, email, dateOfBirth, clientId);
+        
+        //Assert
+        Assert.Equal(false,result);
+    }  
+    
+    [Fact]
+    public void AddUser_Should_Return_False_When_Age_Is_Less_Than_21()
+    {
+        //Arrange
+        string firstName = "John";
+        string lastName = "Wick";
+        DateTime dateOfBirth = new DateTime(2010, 2, 1);
+        int clientId = 5;
+        string email = "wick@wp.pl";
+        var service = new UserService();
+        
+        //Act
+        bool result = service.AddUser(firstName, lastName, email, dateOfBirth, clientId);
+        
+        //Assert
+        Assert.Equal(false,result);
+    } 
+
+
 }
