b548dc587e9e2781dde7bdeba45dfdd66b8e45b4
s27616 <s27616@pjwstk.edu.pl>
2024-04-13 18:31:54 +0200

    usunalem zbedne komentarze

--- Cw3/LegacyApp/UserService.cs
+++ Cw3/LegacyApp/UserService.cs
@@ -44,36 +44,6 @@ namespace LegacyApp
             {
                 return false;
             }
-            // if (client.Type == "VeryImportantClient")
-            // {
-            //     user.HasCreditLimit = false;
-            // }
-            // else if (client.Type == "ImportantClient")
-            // {
-            //     // using (var userCreditService = new UserCreditService())
-            //     // {
-            //     //     int creditLimit = userCreditService.GetCreditLimit(user.LastName, user.DateOfBirth);
-            //     //     creditLimit = creditLimit * 2;
-            //     //     user.CreditLimit = creditLimit;
-            //     // }
-            //     int creditLimit = _userCreditService.GetCreditLimit(user.LastName, user.DateOfBirth);
-            //     creditLimit = creditLimit * 2;
-            //     user.CreditLimit = creditLimit;
-            // }
-            // else
-            // {
-            //     user.HasCreditLimit = true;
-            //     //using (var userCreditService = new UserCreditService())
-            //     //{
-            //     int creditLimit = _userCreditService.GetCreditLimit(user.LastName, user.DateOfBirth); 
-            //     user.CreditLimit = creditLimit;
-            //     //}
-            // }
-            //
-            // if (user.HasCreditLimit && user.CreditLimit < 500)
-            // {
-            //     return false;
-            // }
 
             UserDataAccess.AddUser(user);
             return true;
